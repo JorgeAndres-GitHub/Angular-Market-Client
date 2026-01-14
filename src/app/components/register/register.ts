7fb734df1f940549f094d20f1247265126e5fc1f
Jorge Andres <jorgeandresherreramonsalve@gmail.com>
2026-01-14 14:33:52 -0500

    Making the design for register layout

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [MatFormFieldModule, MatButtonModule, ReactiveFormsModule, MatInputModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
 
+  private formBuilder = new FormBuilder();
+
+  form = this.formBuilder.group({
+    name: ['', {validators: [Validators.required]}],
+    email: ['', {validators: [Validators.required, Validators.email]}],
+    password: ['', {validators: [Validators.required, Validators.minLength(8)]}],
+  });
+
+  getErrorNameField(): string{
+    let name = this.form.controls.name;
+    if(name.hasError('required')){
+      return 'Name is required';
+    }
+
+    return "";
+  }
+
+  getErrorEmailField(): string{
+    let email = this.form.controls.email;
+    if(email.hasError('required')){
+      return 'Email is required';
+    }
+    if(email.hasError('email')){
+      return 'Not a valid email';
+    }
+
+    return "";
+  }
+
+  getErrorPasswordField(): string{
+    let password = this.form.controls.password;
+    if(password.hasError('required')){
+      return 'Password is required';
+    }
+    if(password.hasError('minlength')){
+      return 'Password must be at least 8 characters long';
+    }
+    return "";
+  }
+
+  submitRegistration() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+
+  }
 }
