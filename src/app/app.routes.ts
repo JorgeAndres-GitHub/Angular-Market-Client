251b33f04d72280630c2dc5aee9caa1aa173a98e
Jorge Andres <jorgeandresherreramonsalve@gmail.com>
2026-01-14 14:06:12 -0500

    Adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+import { LandingPage } from './components/landing-page/landing-page';
 
 export const routes: Routes = [
-    
+    {path: '', component: LandingPage},
+    {path: 'login', component: Login},
+    {path: 'register', component: Register} 
 ];
